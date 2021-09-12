b4098b842830464ac61e6258470cec2b75112aec
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-09-11 18:54:27 -0700

    Completed project

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function modelReady(){
 }
 var dog = 0;
 var cat = 0;
+var lion = 0;
 
 function gotResults(error, results) {
   if (error) {
@@ -22,18 +23,21 @@ function gotResults(error, results) {
 
 
     document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
-    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat+ 'Detected Lion - '+lion ;
     document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
     document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 
     img = document.getElementById('animal_image');
-
-    if (results[0].label == "Barking") {
+//lion-smile.gif
+    if (results[0].label == "Wolf") {
       img.src = 'bark.gif';
       dog = dog+1;
-    } else if (results[0].label == "Meowing") {
+    } else if (results[0].label == "Meow") {
       img.src = 'meow.gif';
       cat = cat + 1;
+    } else if (results[0].label == "Roar") {
+      img.src = 'lion-smile.gif';
+      lion = lion + 1;
     } else{
       img.src = 'listen.gif';
     }
